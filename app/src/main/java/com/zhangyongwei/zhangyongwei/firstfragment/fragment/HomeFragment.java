b3d916692a0b2b9f951fa524e54a02bba6550bc2
zhangyongwei <694498731@qq.com>
2017-03-09 15:26:35 +0800

    5.上拉与下拉刷新

--- app/src/main/java/com/zhangyongwei/zhangyongwei/firstfragment/fragment/HomeFragment.java
+++ app/src/main/java/com/zhangyongwei/zhangyongwei/firstfragment/fragment/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.zhangyongwei.zhangyongwei.firstfragment.fragment;
 
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.alibaba.fastjson.JSON;
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
 import com.zhangyongwei.zhangyongwei.R;
 import com.zhangyongwei.zhangyongwei.base.BaseFragment;
 import com.zhangyongwei.zhangyongwei.firstfragment.adapter.HomeAdapter;
@@ -32,7 +35,18 @@ public class HomeFragment extends BaseFragment {
     RecyclerView rvHome;
     @InjectView(R.id.ib_top)
     ImageButton ibTop;
+    @InjectView(R.id.refresh)
+    MaterialRefreshLayout refresh;
+    //适配器
     private HomeAdapter adapter;
+
+    private boolean isLoadMore=false;
+
+    private String moreUrl;
+    private HomeBean homeBean;
+    private List<HomeBean.DataBean.NewsBean> news;
+    private List<HomeBean.DataBean.NewsBean> moreNews;
+
 
     @Override
     public View initView() {
@@ -51,8 +65,64 @@ public class HomeFragment extends BaseFragment {
         super.initData();
         //联网请求数据
         getDataFromNet();
+
+        //初始化监听
+        initListener();
+
     }
 
+    private void initListener() {
+
+        refresh.setMaterialRefreshListener(new MaterialRefreshListener() {
+            @Override
+            public void onRefresh(MaterialRefreshLayout materialRefreshLayout) {
+
+                isLoadMore = false;
+
+                getDataFromNet();
+            }
+
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+
+                isLoadMore = true;
+
+                getMoreDataFromNet(moreUrl);
+            }
+        });
+
+    }
+    /**
+     * 加载更多数据
+     * @param moreUrl
+     */
+    private void getMoreDataFromNet(String moreUrl) {
+
+        OkHttpUtils.get()
+                .url(moreUrl)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "联网失败" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "联网成功" + response);
+
+                        processData(response);
+
+                        refresh.finishRefreshLoadMore();
+                    }
+                });
+
+    }
+
+    /**
+     * 联网请求
+     */
     private void getDataFromNet() {
 
         OkHttpUtils.get()
@@ -70,32 +140,55 @@ public class HomeFragment extends BaseFragment {
                         Log.e("TAG", "联网成功" + response);
 
                         processData(response);
+
+                        refresh.finishRefresh();
                     }
                 });
     }
 
     private void processData(String json) {
-        HomeBean homeBean = JSON.parseObject(json, HomeBean.class);
-        List<HomeBean.DataBean.NewsBean> news = homeBean.getData().getNews();
+        homeBean = JSON.parseObject(json, HomeBean.class);
+        String more = homeBean.getData().getMore();
 
-        //设置适配器
-        adapter = new HomeAdapter(mContext, news);
+        Log.e("TAG", "11111111111111111"+more);
 
-        rvHome.setAdapter(adapter);
+        if(TextUtils.isEmpty(more)) {
 
+            moreUrl="";
+        }else{
 
-        //设置布局管理器
-        rvHome.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
+            moreUrl = Constants.BASE_URL+more;
+        }
 
+        if(!isLoadMore) {
 
+            news = homeBean.getData().getNews();
+            //设置适配器
+            adapter = new HomeAdapter(mContext, news);
+
+            rvHome.setAdapter(adapter);
+
+            //设置布局管理器
+            GridLayoutManager manager = new GridLayoutManager(mContext,1);
+
+            rvHome.setLayoutManager(manager);
+
+        }else{
+
+            moreNews = homeBean.getData().getNews();
+
+            news.addAll(moreNews);
+
+            adapter.notifyDataSetChanged();
+
+        }
 
     }
-
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.reset(this);
     }
+
 }
